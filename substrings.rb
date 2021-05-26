89f62182fa072e3e40c89edaf80e283b9cf9b658
aurelievasseur <aurelie.vasseur@gmail.com>
2021-05-26 19:05:38 -0400

    exercise done

---
+++ substrings.rb
@@ -0,0 +1,19 @@
+def substrings(word, arr)
+    # return a hash listing each substring and how many times it was found 
+    hash_result = {}
+    # pour chaque item de array
+    arr.each_with_index do
+        |val, i| 
+        if word.include?(val)
+            if hash_result[val]
+                hash_result[val] += 1
+            else hash_result[val] = 1
+            end
+        end
+    end
+    puts hash_result
+    return hash_result
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
